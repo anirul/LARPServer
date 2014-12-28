f3740d6b3402ba98c86f2e15792cd2176de98730
Frédéric Dubouchet <frederic.dubouchet@gmail.com>
2014-12-28 16:33:26 +0100

    begin transfer stuff

--- sources/do_stuff.js
+++ sources/do_stuff.js
@@ -1,36 +1,53 @@
 
 
 function Login_Page_Loaded(){
+    my_super_sekkrit_number = 0;
+    Login_Form = window.document.getElementById("LoginForm");
+    stuff = window.document.getElementById("stuff");
+    uname = window.document.getElementById("uname").value;
+    pass = window.document.getElementById("pass").value;
+    Transfer_Form = window.document.getElementById("TransferForm");
+    Transfer_Form.style.display = "none";
+}
 
+
+
+function Login(){
+    var url = "/api/login/?user="+uname+"&pass="+pass;
+    stuff.innerHTML = url;
+    LoginRequest = new XMLHttpRequest();
+    LoginRequest.onreadystatechange=Login_Return;
+    LoginRequest.open("GET",url,true);
+    LoginRequest.send();
 }
 
 function Login_Return(){
-    if (xmlhttp.readyState==4){
+    if (LoginRequest.readyState==4){
 	Login_Form.style.display = "none";
-	if(xmlhttp.status==200){
-	    stuff.innerHTML = "Welcome "+uname;
+	if(LoginRequest.status==200){
+	    stuff.innerHTML = "Welcome "+uname+" you have "+LoginRequest.responseText+" kredits.";
+	    Transfer_Form.style.display = "all";
 	}
-	else if(xmlhttp.status==500||xmlhttp.status==400){
-	    stuff.innerHTML = stuff.innerHTML+" : "+xmlhttp.responseText;
+	else if(LoginRequest.status==500||LoginRequest.status==400){
+	    stuff.innerHTML = LoginRequest.responseText;
 	}
 	else{
-	    stuff.innerHTML="Faute d'erreur! Grave! Grave!!";
+	    stuff.innerHTML="HACKER!!";
 	}
     }
 }
 
 
-function Login(){
-    Login_Form = window.document.getElementById("LoginForm");
-    stuff = window.document.getElementById("stuff");
-    uname = window.document.getElementById("uname").value;
-    pass = window.document.getElementById("pass").value;
-    var url = "/api/login/?user="+uname+"&pass="+pass;
-    stuff.innerHTML = url;
-    xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=Login_Return;
-    xmlhttp.open("GET",url,true);
-    xmlhttp.send();
+function Transfer{
+
+var url = "";
+TransferRequest =  new XMLHttpRequest();
+
+
+
+}
+
+function TransferReturn(){
 
 }
 
